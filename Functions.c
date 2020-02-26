a299efbc0d7e2391df44f344cdc47a17e8f00d1b
Jack Ramsay <61477405+jackramsay1@users.noreply.github.com>
2020-02-26 15:56:10 -0700

    Update Functions.c

--- Functions.c
+++ Functions.c
@@ -1,10 +1,16 @@
-int PlayerCardNumber(){
-
-  return r;
-  }
-  
-int ComputerCardNumer(){
-
-return r;
-
+int RandomPlayerNumberCard(){
+    int r;
+    r=rand() % 14 + 1;
+    
+    return r;
 }
+
+int RandomComputerNumberCard(){
+    int r;
+    r=rand() % 14 + 1;
+    
+    return r;
+}
+
+char RandomSuiteCard(){
+    char suites[4]
