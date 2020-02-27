0795de71b748e1c39bcbbc9d1d459545ebe3604f
Jack Ramsay <61477405+jackramsay1@users.noreply.github.com>
2020-02-27 13:43:19 -0700

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define ROWS 18
+#define COLS 18 
+int Computerchoice(){
+    int r;
+    r= rand() % 9 + 1;
+      return r; 
+  
+  int playerchoice(int x){
+    
+    
+  }
+  
+  
+  char 
+      
+
+
+
 
 int main() {
+  
 
 
 
