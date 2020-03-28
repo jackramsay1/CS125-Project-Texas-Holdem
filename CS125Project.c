ac03c2a31e0d39b74bc67854a8972fb17b34f35d
Jack Ramsay <61477405+jackramsay1@users.noreply.github.com>
2020-03-27 20:04:15 -0700

    Update CS125Project.c

--- CS125Project.c
+++ CS125Project.c
@@ -6,6 +6,34 @@
 void Playerselect(int x, char grid[ROWS][COLUMN]);
 void print_array(char grid[ROWS][COLUMN]);
 void Playerselecto(int o, char grid[ROWS][COLUMN]);
+int testforwin(char grid[ROWS][COLUMN]){
+	int value;
+if( grid[0][5]=='X' && grid[0][10]=='X' && grid[0][14]=='X'|| 
+    grid[0][5]=='X' && grid[2][5]=='X' && grid[4][5]=='X'||
+	grid[0][10]=='X' && grid[2][10]=='X' && grid[4][10]=='X'||
+	grid[0][14]=='X' && grid[2][14]=='X' && grid[4][14]=='X' ||
+	grid[2][5]=='X' && grid[2][10]=='X' && grid[2][14]=='X' ||
+	grid[4][5]=='X' && grid[4][10]=='X' && grid[4][14]=='X' ||
+	grid[0][5]=='X' &&  grid[2][10]=='X' && grid[4][14]=='X' ||
+	grid[4][5]=='X'&& grid[2][10]=='X' && grid[0][14]=='X')  {
+	
+		value=1;
+	}
+	else if(grid[0][5]=='O' && grid[0][10]=='O' && grid[0][14]=='O' ||
+			grid[0][5]=='O' && grid[2][5]=='O' && grid[4][5]=='O'||
+			grid[0][10]=='O' && grid[2][10]=='O' && grid[4][10]=='O'||
+			grid[0][14]=='O' && grid[2][14]=='O' && grid[4][14]=='O' ||
+			grid[2][5]=='O' && grid[0][10]=='O' && grid[0][14]=='O' ||
+			grid[4][5]=='O' && grid[4][10]=='O' && grid[4][14]=='O' ||
+			grid[0][5]=='O' &&  grid[2][10]=='O' && grid[4][14]=='O'||
+			grid[4][5]=='O'&& grid[2][10]=='O' && grid[0][14]=='O') 
+	{
+		value=2;
+	}
+	else 
+		value=0;
+	return value;
+}
 int Computerchoice(void){
     int r;
     r=rand()%9+1;
@@ -13,14 +41,14 @@ int Computerchoice(void){
     }
 void playerinput(char grid[ROWS][COLUMN]){
     int x;
-    printf("enter your position to move(x): ");
+    printf("enter your position to move(X): ");
     scanf("%d",&x);
     Playerselect(x,grid);
     print_array(grid);
     }
 void playerinputo(char grid[ROWS][COLUMN]){
     int o;
-    printf("enter your position to move(o): ");
+    printf("enter your position to move(O): ");
     scanf("%d",&o);
     Playerselecto(o,grid);
     print_array(grid);
@@ -29,64 +57,207 @@ void playerinputo(char grid[ROWS][COLUMN]){
 void Playerselect(int x,char grid[ROWS][COLUMN]){
         switch(x){
         case 1:
+		while(1){
+			if (grid[0][5]=='O' || grid[0][5]=='X'){
+				printf("This position is taken\n");
+				playerinput(grid);
+			}
+			else 
         grid[0][5]='X';
+		break;
+		};
         break;
         case 2:
+		while(1){
+			if (grid[0][10]=='O' || grid[0][10]=='X'){
+				printf("This position is taken \n");
+				playerinput(grid);
+			}
+			else 
         grid[0][10]='X';
+		break;
+		};
         break;
         case 3:
+		while(1){
+			if (grid[0][14]=='O' || grid[0][14]=='X'){
+				printf("This position is taken \n");
+				playerinput(grid);
+			}
+			else 
         grid[0][14]='X';
+		break;
+		};
         break;
         case 4:
+		while(1){
+			if (grid[2][5]=='O'|| grid[2][5]=='X'){
+				printf("This position is taken \n");
+				playerinput(grid);
+			}
+			else 
         grid[2][5]='X';
+		break;
+		};
         break;
         case 5:
-        grid[2][10]='x';    
+		while(1){
+			if (grid[2][10]=='O' || grid[2][10]=='X'){
+				printf("This position is taken by player O\n");
+				playerinput(grid);
+			}
+			else 
+        grid[2][10]='X';
+		break;
+		};
         break;
         
         case 6:
+		while(1){
+			if (grid[2][14]=='O' || grid[2][14]=='X'){
+				printf("This position is taken\n");
+				playerinput(grid);
+			}
+			else 
         grid[2][14]='X';
+		break;
+		};
         break;
         case 7:
+		while(1){
+			if (grid[4][5]=='O' || grid[4][5]=='X'){
+				printf("This position is taken\n");
+				playerinput(grid);
+			}
+			else 
         grid[4][5]='X';
+		break;
+		};
         break;
         case 8:
+		while(1){
+			if (grid[4][10]=='O' || grid[4][10]=='X'){
+				printf("This position is taken \n");
+				playerinput(grid);
+			}
+			else 
         grid[4][10]='X';
+		break;
+		};
         break;
         case 9:
+		while(1){
+			if (grid[4][14]=='O'|| grid[4][14]=='X'){
+				printf("This position is taken\n");
+				playerinput(grid);
+			}
+			else 
         grid[4][14]='X';
+		break;
+		};
         break;
         }
 }
 void Playerselecto(int o,char grid[ROWS][COLUMN]){
 	switch(o){
         case 1:
+		while(1){
+			if (grid[0][5]=='X' || grid[0][5]=='O'){
+				printf("This position is taken \n");
+				playerinputo(grid);
+			}
+			else 
         grid[0][5]='O';
+		break;
+		};
         break;
         case 2:
+		while(1){
+			if (grid[0][10]=='X' || grid[0][10]=='O'){
+				printf("This position is taken \n");
+				playerinputo(grid);
+			}
+			else 
         grid[0][10]='O';
+		break;
+		};
         break;
         case 3:
+		while(1){
+			if (grid[0][14]=='X' || grid[0][14]=='O'){
+				printf("This position is taken\n");
+				playerinputo(grid);
+			}
+			else 
         grid[0][14]='O';
+		break;
+		};
         break;
         case 4:
+		while(1){
+ 	if (grid[2][5]=='X'|| grid[2][5]=='O'){
+				printf("This position is taken\n");
+				playerinputo(grid);
+			}
+			else 
         grid[2][5]='O';
+		break;
+		};
         break;
         case 5:
-        grid[2][10]='O';    
+        while(1){
+ 	if (grid[2][10]=='X' || grid[2][10]=='O'){
+				printf("This position is taken\n");
+				playerinputo(grid);
+			}
+			else 
+        grid[2][10]='O';
+		break;
+		}; 
         break;
-        
         case 6:
+			while(1){
+				if (grid[2][14]=='X' || grid[2][14]=='O'){
+				printf("This position is taken\n");
+				playerinputo(grid);
+			}
+			else 
         grid[2][14]='O';
+		break;
+		};
         break;
         case 7:
+		while(1){
+			if (grid[4][5]=='X' || grid[4][5]=='O'){
+				printf("This position is taken \n");
+				playerinputo(grid);
+			}
+			else 
         grid[4][5]='O';
+		break;
+		};
         break;
         case 8:
+		while(1){
+			if (grid[4][10]=='X'|| grid[4][10]=='O'){
+				printf("This position is taken\n");
+				playerinputo(grid);
+			}
+			else 
         grid[4][10]='O';
+		break;
+		};
         break;
         case 9:
+		while(1){
+			if (grid[4][14]=='X' || grid[4][14]=='O'){
+				printf("This position is taken\n");
+				playerinputo(grid);
+			}
+			else 
         grid[4][14]='O';
+		break;
+		};
         break;
         }
 }
@@ -104,18 +275,40 @@ void Playerselecto(int o,char grid[ROWS][COLUMN]){
          }
 
 int main() {
+	int over=0;
         char grid[ROWS][COLUMN]={
-"     1    2   3",
-"               ",
-"     4    5   6",
-"               ",
-"     7    8   9",
+"     1  | 2 | 3",
+" -------------------",
+"     4  | 5 | 6",
+" -------------------",
+"     7  | 8 | 9",
 };
   printf("\n");
-  print_array(grid);  
-  playerinput(grid);
-  playerinputo(grid);
-  playerinput(grid);
-  playerinputo(grid);  
+
+  while(1){
+	print_array(grid);  
+	playerinput(grid);
+	printf("\n");
+	over=testforwin(grid);
+	if(over==1){
+		printf("Player X won\n");
+		break;
+	}
+	else if(over==2){
+		("Player O won\n");
+	break;
+	}
+	playerinputo(grid);
+	printf("\n");
+	over=testforwin(grid);
+	if(over==1){
+		printf("Player X won\n");
+		break;
+	}
+	else if(over==2){
+		printf("Player O won\n");
+	break;
+	}
+  }
 return 0;
 }
